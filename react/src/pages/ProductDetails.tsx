3ce90554067ca4e329f8975028353e5d7a735974
serggunnya <baranov.serg.it@gmail.com>
2023-10-26 14:42:14 +0500

    add product details page

---
+++ react/src/pages/ProductDetails.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import { useGetAllCategoriesQuery } from 'store/slices/productSlice';
+
+const ProductDetails: React.FC = () => {
+  // const { data, isLoading, error } = useGetAllCategoriesQuery({});
+
+  return (
+    <div>
+      <div>О Товаре</div>
+    </div>
+  );
+};
+
+export default ProductDetails;
