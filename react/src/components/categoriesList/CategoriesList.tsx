9d6fe0d239d916cc185c95ea4166439ebd5dd42d
serggunnya <baranov.serg.it@gmail.com>
2023-11-04 14:04:01 +0500

    add category list select style

--- react/src/components/categoriesList/CategoriesList.tsx
+++ react/src/components/categoriesList/CategoriesList.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { createSearchParams, useNavigate } from 'react-router-dom';
+import {
+  createSearchParams,
+  useSearchParams,
+  useNavigate,
+} from 'react-router-dom';
 import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 
 import { useGetAllCategoriesQuery } from 'store/slices/productSlice';
 import CategoriesSkeleton from './CategoriesSkeleton';
 
+import styles from './categoriesList.module.css';
+
 const CategoriesList: React.FC = (props) => {
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const { data, isLoading, error } = useGetAllCategoriesQuery({});
+
+  const isActive = (id: number) => Number(searchParams?.get('cat_id')) === id;
 
   const goTo = (id: number) => () =>
     navigate({
@@ -24,7 +33,11 @@ const CategoriesList: React.FC = (props) => {
         <div>
           <List>
             {data.map((cat: any) => (
-              <ListItem key={cat.id} disablePadding>
+              <ListItem
+                key={cat.id}
+                disablePadding
+                className={isActive(cat.id) ? styles.itemActive : styles.item}
+              >
                 <ListItemButton onClick={goTo(cat.id)}>
                   <ListItemText primary={cat.name} />
                 </ListItemButton>
