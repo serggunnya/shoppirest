948c4fe4dcd4e23755a162d3c0c0a3ac0b5f9751
serggunnya <baranov.serg.it@gmail.com>
2024-04-07 16:54:35 +0500

    eslint and prettier settings

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,13 +1,19 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '',
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    open: true,
-    port: 3000,
-  },
+	// depending on your application, base can also be "/"
+	resolve: {
+		alias: {
+			components: "./src/components",
+			pages: "./src/pages",
+			store: "./src/store",
+		},
+	},
+	plugins: [react(), viteTsconfigPaths()],
+	server: {
+		open: true,
+		port: 3000,
+	},
 });
