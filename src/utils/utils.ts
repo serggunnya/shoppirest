796e629b5cba5721423a8ea967a69432345c1388
serggunnya <baranov.serg.it@gmail.com>
2024-05-21 19:09:02 +0500

    toWhere fix separator

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,26 +1,26 @@
 interface IQuery {
-  [key: string]: string | number;
+	[key: string]: string | number;
 }
 
 export const toWhere = (cat_id: number, filters: IQuery): any => {
-  const result: any = { cat_id };
+	const result: any = { cat_id };
 
-  if (Object.keys(filters).length) {
-    const entries = Object.entries(filters);
-    const queries = [];
-    for (const props of entries) {
-      const [attr, values] = props;
+	if (Object.keys(filters).length) {
+		const entries = Object.entries(filters);
+		const queries = [];
+		for (const props of entries) {
+			const [attr, values] = props;
 
-      const properties = {
-        some: {
-          attribute_alias: { equals: attr },
-          AND: { option: { alias: { in: String(values).split('+') } } },
-        },
-      };
-      queries.push({ properties });
-    }
-    result.OR = queries;
-  }
+			const properties = {
+				some: {
+					attribute_alias: { equals: attr },
+					AND: { option: { alias: { in: String(values).split(",") } } },
+				},
+			};
+			queries.push({ properties });
+		}
+		result.AND = queries;
+	}
 
-  return result;
+	return result;
 };
