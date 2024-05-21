915e22a0e47d3fe627572a612272ff59dc8aa0ca
serggunnya <baranov.serg.it@gmail.com>
2024-05-21 19:09:37 +0500

    add photos to products

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,50 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { toWhere } from 'utils/utils';
-import { PrismaService } from '../prisma/prisma.service';
-import { ISearchParams } from './product.types';
+import { Injectable } from "@nestjs/common";
+import { toWhere } from "utils/utils";
+
+import { PrismaService } from "../prisma/prisma.service";
+import { ISearchParams } from "./product.types";
 
 // const util = require('util');
 
 @Injectable()
 export class ProductService {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
 
-  async getProductsByCatId({
-    page = 1,
-    limit = 5,
-    cat_id,
-    ...filters
-  }: ISearchParams) {
-    const query = toWhere(Number(cat_id), filters);
+	async getProductsByCatId({ page = 1, limit = 5, cat_id, ...filters }: ISearchParams) {
+		const query = toWhere(Number(cat_id), filters);
 
-    const pageOffset = page > 0 ? limit * (page - 1) : 0;
+		const pageOffset = page > 0 ? limit * (page - 1) : 0;
 
-    const [total, data] = await Promise.all([
-      this.prisma.product.count({
-        where: query,
-      }),
-      this.prisma.product.findMany({
-        where: query,
-        take: Number(limit),
-        skip: pageOffset,
-      }),
-    ]);
+		const [total, data] = await Promise.all([
+			this.prisma.product.count({
+				where: query,
+			}),
+			this.prisma.product.findMany({
+				where: query,
+				take: Number(limit),
+				skip: pageOffset,
+				include: { images: true },
+			}),
+		]);
 
-    return {
-      data,
-      meta: {
-        total,
-        limit,
-        currentPage: page,
-        lastPage: Math.ceil(total / limit),
-      },
-    };
-  }
+		return {
+			data,
+			meta: {
+				total,
+				limit,
+				currentPage: page,
+				lastPage: Math.ceil(total / limit),
+			},
+		};
+	}
 
-  async getProductById(id: number) {
-    return await this.prisma.product.findMany({
-      include: { properties: { include: { option: true } } },
-      where: { id },
-    });
-  }
+	async getProductById(id: number) {
+		return await this.prisma.product.findMany({
+			include: { properties: { include: { option: true } } },
+			where: { id },
+		});
+	}
 }
