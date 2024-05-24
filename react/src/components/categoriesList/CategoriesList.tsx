698d276f694604095c524ed900d8095ddca03d5e
serggunnya <baranov.serg.it@gmail.com>
2024-05-24 16:38:22 +0500

    fix path

--- react/src/components/categoriesList/CategoriesList.tsx
+++ react/src/components/categoriesList/CategoriesList.tsx
@@ -1,53 +1,46 @@
-import React from 'react';
-import {
-  createSearchParams,
-  useSearchParams,
-  useNavigate,
-} from 'react-router-dom';
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import React from "react";
 
-import { useGetAllCategoriesQuery } from 'store/slices/productSlice';
-import CategoriesSkeleton from './CategoriesSkeleton';
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { createSearchParams, useNavigate, useSearchParams } from "react-router-dom";
 
-import styles from './categoriesList.module.css';
+import { useGetAllCategoriesQuery } from "@/store/slices/productSlice";
+
+import CategoriesSkeleton from "./CategoriesSkeleton";
+import styles from "./categoriesList.module.css";
 
 const CategoriesList: React.FC = (props) => {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+	const navigate = useNavigate();
+	const [searchParams, setSearchParams] = useSearchParams();
 
-  const { data, isLoading, error } = useGetAllCategoriesQuery({});
+	const { data, isLoading, error } = useGetAllCategoriesQuery({});
 
-  const isActive = (id: number) => Number(searchParams?.get('cat_id')) === id;
+	const isActive = (id: number) => Number(searchParams?.get("cat_id")) === id;
 
-  const goTo = (id: number) => () =>
-    navigate({
-      pathname: '/products',
-      search: createSearchParams({ page: '1', cat_id: String(id) }).toString(),
-    });
+	const goTo = (id: number) => () =>
+		navigate({
+			pathname: "/products",
+			search: createSearchParams({ page: "1", cat_id: String(id) }).toString(),
+		});
 
-  return (
-    <div>
-      {isLoading ? (
-        <CategoriesSkeleton />
-      ) : (
-        <div>
-          <List>
-            {data.map((cat: any) => (
-              <ListItem
-                key={cat.id}
-                disablePadding
-                className={isActive(cat.id) ? styles.itemActive : styles.item}
-              >
-                <ListItemButton onClick={goTo(cat.id)}>
-                  <ListItemText primary={cat.name} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{isLoading ? (
+				<CategoriesSkeleton />
+			) : (
+				<div>
+					<List>
+						{data.map((cat: any) => (
+							<ListItem key={cat.id} disablePadding className={isActive(cat.id) ? styles.itemActive : styles.item}>
+								<ListItemButton onClick={goTo(cat.id)}>
+									<ListItemText primary={cat.name} />
+								</ListItemButton>
+							</ListItem>
+						))}
+					</List>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default CategoriesList;
