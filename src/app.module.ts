ee00a1022628f25d889e3ace1b502f842b449d59
serggunnya <baranov.serg.it@gmail.com>
2024-11-20 16:18:29 +0500

    add versioning to api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { CategoryModule } from "category/category.module";
+import { join } from "path";
 
-import { CategoryModule } from 'category/category.module';
-import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ProductModule } from './product/product.module';
+import { AuthModule } from "./auth/auth.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { ProductModule } from "./product/product.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-    ProductModule,
-    CategoryModule,
-    AuthModule,
-    PrismaModule,
-  ],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, "..", "images"),
+		}),
+		ProductModule,
+		CategoryModule,
+		AuthModule,
+		PrismaModule,
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,36 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const PORT = 5000;
-  const app = await NestFactory.create(AppModule);
+	const PORT = 5000;
+	const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('/api/v1');
-  app.enableCors({ origin: '*' });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.setGlobalPrefix("/api/");
+	app.enableVersioning({ type: VersioningType.URI });
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.enableCors({ origin: "*" });
 
-  const config = new DocumentBuilder()
-    .setTitle('Shoppirest')
-    .setDescription('The e-commerce API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+	const config = new DocumentBuilder()
+		.setTitle("Shoppirest")
+		.setDescription("The e-commerce API")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.build();
 
-  const document = SwaggerModule.createDocument(app, config);
+	const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: {
-      tagsSorter: 'alpha',
-      operationsSorter: 'alpha',
-    },
-  });
+	SwaggerModule.setup("docs", app, document, {
+		swaggerOptions: {
+			tagsSorter: "alpha",
+			operationsSorter: "alpha",
+		},
+	});
 
-  await app.listen(PORT, () => {
-    console.log(`server started on localhost:${PORT}`);
-  });
+	await app.listen(PORT, () => {
+		console.log(`server started on localhost:${PORT}`);
+	});
 }
 bootstrap();
