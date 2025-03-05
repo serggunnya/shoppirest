52da9f71897e75aa128e24e39768a3dbfa663979
serggunnya <baranov.serg.it@gmail.com>
2025-03-05 18:11:36 +0500

    refactor product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,111 +1,213 @@
-import { Injectable } from "@nestjs/common";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Inject, Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+import { Cache } from "cache-manager";
 import combineFilters from "utils/combineFilters";
 
 import { PrismaService } from "../prisma/prisma.service";
+import {
+	IAttributeEntity,
+	IFacetsResponse,
+	INumericOptionEntity,
+	IProductEntity,
+	IProductsResponse,
+	ITextOptionEntity,
+} from "./product.types";
+
+var hash = require("object-hash");
 
 @Injectable()
 export class ProductService {
-	constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService, @Inject(CACHE_MANAGER) private cacheService: Cache) {}
 
+	//---------------------------------------------------------------------------
 	async getProductsByCategoryIdV1(
 		{ cat_id, page = 1, limit = 5 },
-		filters: { [key: string]: string },
+		filters: { [key: string]: any },
 	) {
-		const test: { [key: string]: string[] } = {
-			ram: ["2gb", "3gb", "4gb", "8gb"],
-			storage: ["16gb", "128gb"],
-		};
+		let withFilters: Prisma.Sql;
+		if (Object.keys(filters).length !== 0) {
+			withFilters = await this._withFilters(Number(cat_id), filters);
+		}
 
-		const conditions = combineFilters(test);
-		const pageOffset = page > 0 ? limit * (page - 1) : 0;
+		const offset = page > 0 ? Number(limit) * (page - 1) : 0;
 
-		const products = await this.prisma.$queryRaw`
-			SELECT * FROM "Product" p 
-			WHERE p.cat_id = ${Number(cat_id)} 
-			${conditions} 
-			LIMIT ${Number(limit)} OFFSET ${Number(pageOffset)}`;
-
-		const productsTotal = await this.prisma.$queryRaw`
-			SELECT count(p.id)::int FROM "Product" p 
-			WHERE p.cat_id = ${Number(cat_id)} 
-			${conditions} 
-			LIMIT ${Number(limit)} OFFSET ${Number(pageOffset)}`;
+		const [products, total] = await Promise.all([
+			this._getProducts(Number(cat_id), withFilters, offset, Number(limit)),
+			this._getProductsCount(Number(cat_id), withFilters),
+		]);
 
 		return {
 			products,
 			meta: {
-				total: productsTotal[0].count,
+				total: total[0].count,
 				limit: Number(limit),
 				currentPage: Number(page),
-				lastPage: Math.ceil(Number(productsTotal[0].count) / Number(limit)),
+				lastPage: Math.ceil(total[0].count / Number(limit)),
 			},
-		};
+		} as IProductsResponse;
 	}
 
-	//todo
-	async getProductsByCategoryIdV2(
-		{ cat_id, page = 1, limit = 5 },
-		filters: { [key: string]: string },
-	) {
-		return "get products from elastic";
-	}
+	//---------------------------------------------------------------------------
 
-	async getFacetsByCatIdV1({ cat_id }, filters: { [key: string]: string }) {
-		// get all attributes for category id
-		const attributes: any = await this.prisma.$queryRaw`
-			select attr.*
-			from "Product" p
-			JOIN "Product_property" pp ON pp.prod_id = p.id
-			JOIN "Attribute" attr ON pp.attr_alias = attr.alias
-			WHERE p.cat_id = ${Number(cat_id)}
-			GROUP BY attr.id`;
-
-		type TOptions = {
-			id: Number;
-			attr_alias: String;
-			option_value: String;
-			option_alias: String;
-			count: BigInt;
-		};
-
-		// get all facets for category id
-		const options: TOptions[] = await this.prisma.$queryRaw`
-			select
-			pp.attr_alias,
-			pp.option_value,
-			pp.option_alias,
-			count(pp.option_value)::int
-			from "Product" p
-			JOIN "Product_property" pp ON pp.prod_id = p.id
-			WHERE p.cat_id = ${Number(cat_id)}
-			GROUP by pp.attr_alias, pp.option_value, pp.option_alias`;
-
-		// get map of options arrays
-		const optionsMap = new Map();
-		for (let o of options) {
-			if (!optionsMap.has(o.attr_alias)) {
-				optionsMap.set(o.attr_alias, []);
-			}
-			optionsMap.get(o.attr_alias).push(o);
+	async getFacetsByCategoryIdV1({ cat_id }, filters: { [key: string]: any }) {
+		let withFilters: Prisma.Sql;
+		if (Object.keys(filters).length !== 0) {
+			withFilters = await this._withFilters(Number(cat_id), filters);
 		}
 
-		// // mapping options list to attribute
+		const attributes = await this._getAttributes(Number(cat_id));
+
+		attributes.push({
+			id: 0,
+			name: "Цена",
+			alias: "price",
+			type: 1,
+			createdAt: new Date(),
+		});
+
+		const options = [
+			...(await this._getTextOptions(Number(cat_id), withFilters)),
+			...(await this._getNumericOptions(Number(cat_id))),
+		];
+
+		const attrMap = new Map();
 		for (let attr of attributes) {
-			attr.options = optionsMap.get(attr.alias);
+			(attr as IFacetsResponse).options = [];
+			attrMap.set(attr.alias, attr);
 		}
+
+		for (let opt of options) {
+			attrMap.get(opt.alias).options.push(opt);
+		}
+
+		return attributes as IFacetsResponse[];
+	}
+
+	//---------------------------------------------------------------------------
+	async getProductById(id: number) {
+		return await this.prisma.product.findMany({ where: { id } });
+	}
+
+	//---------------------------------------------------------------------------
+	async _getAttributes(cat_id: number): Promise<IAttributeEntity[]> {
+		const cachedAttributes = await this.cacheService.get<IAttributeEntity[]>(
+			`${cat_id}::attributes`,
+		);
+
+		if (cachedAttributes) {
+			return cachedAttributes;
+		}
+
+		const attributes: IAttributeEntity[] = await this.prisma.$queryRaw`
+			SELECT a.* FROM "Attribute" a
+			JOIN "Category_attribute" ca on ca.attribute_id = a.id
+			where ca.category_id = ${cat_id} GROUP BY a.id
+			`;
+
+		await this.cacheService.set(`${cat_id}::attributes`, attributes, 1000 * 60 * 10);
 
 		return attributes;
 	}
 
-	//todo
-	async getFacetsByCatIdV2({ cat_id }, filters: { [key: string]: string }) {
-		return "facets from elastic";
+	//---------------------------------------------------------------------------
+	async _getTextOptions(cat_id: number, withFilters): Promise<ITextOptionEntity[]> {
+		if (withFilters !== undefined) {
+			return this.prisma.$queryRaw`
+				WITH ids AS (${withFilters})
+				SELECT a.id, p.* FROM (
+					SELECT "alias", "value", COUNT(CASE WHEN p.id = ids.id THEN 1 END)::int
+					FROM "Product" p
+					JOIN LATERAL jsonb_each_text(p.properties) AS attr("alias") ON TRUE
+					LEFT JOIN ids ON p.id = ids.id
+					where p.cat_id = ${cat_id} 
+					GROUP BY alias, value
+				) AS p
+				JOIN "Attribute" a ON p.alias = a.alias AND a."type" in (0,1);
+				`;
+		}
+
+		return this.prisma.$queryRaw`			
+			SELECT a.id, p.* FROM (
+    		SELECT "alias", "value", COUNT(p.id)::int
+    		FROM "Product" p
+    		JOIN LATERAL jsonb_each_text(p.properties) AS attr("alias") ON TRUE    		
+    		where p.cat_id = ${cat_id} 
+    		GROUP BY alias, value
+			) AS p
+			JOIN "Attribute" a ON p.alias = a.alias AND a."type" in (0,1);
+			`;
 	}
 
-	async getProductById(id: number) {
-		return await this.prisma.product.findMany({
-			include: { properties: true },
-			where: { id },
-		});
+	//---------------------------------------------------------------------------
+	async _getNumericOptions(cat_id: number): Promise<INumericOptionEntity[]> {
+		const cachedOptions = await this.cacheService.get<INumericOptionEntity[]>(`${cat_id}::ranges`);
+
+		if (cachedOptions) {
+			return cachedOptions;
+		}
+
+		const numericOptions: INumericOptionEntity[] = await this.prisma.$queryRaw`
+			select a.id, p.alias, min(p.value), max(p.value) from (
+				select "alias", "value"    
+				FROM "Product" p, jsonb_each_text(p.properties) as attr("alias")
+				where p.cat_id = ${cat_id}
+				GROUP by alias, value
+			) as p
+			join "Attribute" a on p.alias = p.alias
+			and a.alias = p.alias and a."type" in (2,3)
+			group by p.alias, a.id
+			union			
+			select 0 as id, 'price' as "alias", min(p.price)::text, max(p.price)::text price 
+			from "Product" p where p.cat_id = ${cat_id}
+			`;
+
+		await this.cacheService.set(`${cat_id}::ranges`, numericOptions, 1000 * 60 * 10);
+
+		return numericOptions;
+	}
+
+	//---------------------------------------------------------------------------
+	async _withFilters(cat_id: number, filters: { [key: string]: any }): Promise<Prisma.Sql> {
+		const hashKey = hash(filters);
+		const withIdsCached: Prisma.Sql = await this.cacheService.get<Prisma.Sql>(hashKey);
+
+		if (withIdsCached) {
+			return withIdsCached;
+		}
+
+		const withIds = combineFilters(cat_id, filters);
+		await this.cacheService.set(hashKey, withIds, 1000 * 60 * 10);
+		return withIds;
+	}
+
+	//---------------------------------------------------------------------------
+	async _getProducts(cat_id: number, withFilters, offset, limit): Promise<IProductEntity[]> {
+		if (withFilters !== undefined) {
+			return await this.prisma.$queryRaw`
+				WITH ids AS (${withFilters})
+				select * from "Product" p 
+				join ids on ids.id = p.id
+				where p.cat_id = ${cat_id}
+				limit ${limit} offset ${offset}
+				`;
+		}
+
+		return await this.prisma.$queryRaw`			
+			select * from "Product" p 
+			where p.cat_id = ${cat_id}
+			limit ${limit} offset ${offset}
+			`;
+	}
+
+	//---------------------------------------------------------------------------
+	async _getProductsCount(cat_id: number, withFilters): Promise<{ count: number }[]> {
+		if (withFilters !== undefined) {
+			return await this.prisma.$queryRaw`select count(id)::int from (${withFilters}) as pp`;
+		}
+
+		return await this.prisma.$queryRaw`
+			select count(id)::int from "Product" p where p.cat_id = ${cat_id}
+		`;
 	}
 }
