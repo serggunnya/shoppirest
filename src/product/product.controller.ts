141b88101753c006fbf7728c1465622489fcdebb
serggunnya <baranov.serg.it@gmail.com>
2025-03-05 18:10:42 +0500

    refactor product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,54 +1,69 @@
-import { Body, Controller, Get, Param, Query, Version } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query, Version } from "@nestjs/common";
 import { ParseIntPipe } from "@nestjs/common/pipes";
-import { ApiOkResponse, ApiParam, ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
 
-import { ProductEntity } from "./ProductEntity";
+import { ProductEntityDoc, ProductResponseDoc } from "./Product.Entity";
 import { ProductService } from "./product.service";
 import { IProductParams } from "./product.types";
+
+const example = `{
+			"minPrice": 0,
+			"maxPrice": 130599,
+			"ram": ["2gb", "3gb", "4gb", "8gb"],
+			"storage": ["16gb", "128gb"],
+			...
+		}`;
 
 @ApiTags("Products")
 @Controller("products")
 export class ProductController {
 	constructor(private productService: ProductService) {}
 
+	//---------------------------------------------------------------------------
+	@Post("/")
 	@Version("1")
-	@Get("/")
-	@ApiOkResponse({ type: ProductEntity, isArray: true })
+	@ApiQuery({ name: "cat_id", required: true })
+	@ApiQuery({ name: "page", required: true })
+	@ApiQuery({ name: "limit", required: false })
+	@ApiBody({
+		required: false,
+		description: `Product filters example ${example}`,
+		schema: { type: `object` },
+	})
+	@ApiOkResponse({ type: ProductResponseDoc })
+	@ApiOperation({ description: "Get list of all or filtered products", operationId: "1" })
 	getProductsByCategoryIdV1(
 		@Query() params: IProductParams,
-		@Body() filters: { [key: string]: string },
+		@Body() filters: { [key: string]: any },
 	) {
 		return this.productService.getProductsByCategoryIdV1(params, filters);
 	}
 
-	@Version("2")
-	@Get("/")
-	@ApiOkResponse({ type: ProductEntity, isArray: true })
-	getProductsByCategoryIdV2(
-		@Query() params: IProductParams,
-		@Body() filters: { [key: string]: string },
-	) {
-		return this.productService.getProductsByCategoryIdV2(params, filters);
-	}
-
+	//---------------------------------------------------------------------------
+	@Post("/facets")
 	@Version("1")
-	@Get("/facets")
-	@ApiOkResponse({ type: ProductEntity, isArray: true })
-	getFacetsByCatIdV1(@Query() params: IProductParams, @Body() filters: { [key: string]: string }) {
-		return this.productService.getFacetsByCatIdV1(params, filters);
+	@ApiQuery({ name: "cat_id", required: true })
+	@ApiBody({
+		required: false,
+		description: `Product facets example ${example}`,
+		schema: { type: `object` },
+	})
+	// @ApiOkResponse({ type: FacetsResponseDoc, isArray: true })
+	@ApiOperation({ description: "Get list of all or filtered attributes with available options" })
+	getFacetsByCategoryIdV1(
+		@Query() params: IProductParams,
+		@Body() filters: { [key: string]: any },
+	) {
+		return this.productService.getFacetsByCategoryIdV1(params, filters);
 	}
 
-	@Version("2")
-	@Get("/facets")
-	@ApiOkResponse({ type: ProductEntity, isArray: true })
-	getFacetsByCatIdV2(@Query() params: IProductParams, @Body() filters: { [key: string]: string }) {
-		return this.productService.getFacetsByCatIdV2(params, filters);
-	}
-
+	//---------------------------------------------------------------------------
 	@Get(":id")
-	@ApiParam({ name: "id", required: true })
-	@ApiOkResponse({ type: ProductEntity })
-	productByid(@Param("id", ParseIntPipe) id) {
+	@Version("1")
+	@ApiQuery({ name: "id", required: true })
+	@ApiOkResponse({ type: ProductEntityDoc })
+	@ApiOperation({ description: "Get product by Id" })
+	getProductById(@Param("id", ParseIntPipe) id) {
 		return this.productService.getProductById(id);
 	}
 }
