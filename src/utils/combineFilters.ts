46d4994682a7f73882359d1b30374fc8684f871f
serggunnya <baranov.serg.it@gmail.com>
2025-03-05 18:04:18 +0500

    combine filters update

--- src/utils/combineFilters.ts
+++ src/utils/combineFilters.ts
@@ -1,18 +1,61 @@
 import { Prisma } from "@prisma/client";
 
-const combineFilters = (filters: { [key: string]: string[] }): Prisma.Sql => {
-	const props: [string, string[]][] = Object.entries(filters);
-	if (props.length === 0) return Prisma.empty;
+const combineFilters = (cat_id: number, filters: { [key: string]: any }): Prisma.Sql => {
+	const keys = Object.keys(filters);
+	const conditions: Prisma.Sql[] = [Prisma.empty];
 
-	const conditions: Prisma.Sql[] = [];
-	for (let [attr, val] of props) {
-		const query = Prisma.sql`p.id = ANY (
-		SELECT prod_id FROM "Product_property" pp WHERE 
-		pp.attr_alias=${attr} AND pp.option_alias IN (${Prisma.join(val)}))`;
-		conditions.push(query);
+	for (let i = 0; i < keys.length; i++) {
+		const attr = keys[i];
+
+		if (attr !== "price") {
+			if (filters[attr]?.type === 0) {
+				const query = Prisma.sql`
+					(p.properties ->> ${attr})::boolean IN (${Prisma.join(filters[attr].val)})
+				`;
+				conditions.push(query);
+			} else if (filters[attr]?.type === 1) {
+				const query = Prisma.sql`
+					(p.properties ->> ${attr})::text IN (${Prisma.join(filters[attr].val)})
+				`;
+				conditions.push(query);
+			} else if (filters[attr]?.type === 2) {
+				const query = Prisma.sql`
+					${
+						filters[attr]?.min
+							? Prisma.sql`(p.properties ->> ${attr})::int >= ${filters[attr].min}`
+							: Prisma.empty
+					}
+					${
+						filters[attr]?.max
+							? Prisma.sql`(p.properties ->> ${attr})::int <= ${filters[attr].max}`
+							: Prisma.empty
+					}
+				`;
+				conditions.push(query);
+			} else {
+				const query = Prisma.sql`
+					${
+						filters[attr]?.min
+							? Prisma.sql`(p.properties ->> ${attr})::float >= ${filters[attr].min}`
+							: Prisma.empty
+					}
+					${
+						filters[attr]?.max
+							? Prisma.sql`(p.properties ->> ${attr})::float <= ${filters[attr].max}`
+							: Prisma.empty
+					}
+				`;
+				conditions.push(query);
+			}
+		}
 	}
 
-	return Prisma.sql`AND ${Prisma.join(conditions, " AND ")}`;
+	return Prisma.sql`
+		SELECT id FROM "Product" p where p.cat_id = ${cat_id}
+		${filters.price?.min ? Prisma.sql`AND p.price >= ${filters.price.min}` : Prisma.empty}
+		${filters.price?.max ? Prisma.sql`AND p.price <= ${filters.price.max}` : Prisma.empty}		
+		${Prisma.join(conditions, "AND")}
+	`;
 };
 
 export default combineFilters;
