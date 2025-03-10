ae88afe3bcbfbb3516c0ce41dbe7dd25947ca5ca
serggunnya <baranov.serg.it@gmail.com>
2025-03-10 18:25:14 +0500

    refactor eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,51 @@ module.exports = {
 		project: "tsconfig.json",
 		tsconfigRootDir: __dirname,
 		sourceType: "module",
+		ecmaVersion: 2020,
 	},
-	plugins: ["@typescript-eslint/eslint-plugin", "react"],
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended"],
+	plugins: ["@typescript-eslint/eslint-plugin", "react", "prettier", "import", "react-hooks"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"plugin:prettier/recommended",
+		"plugin:import/typescript",
+		"plugin:import/errors",
+	],
 	root: true,
 	env: {
 		node: true,
 		jest: true,
+		es2020: true,
 	},
-	ignorePatterns: [".eslintrc.js"],
+	settings: {
+		react: {
+			version: "detect",
+		},
+		"import/resolver": {
+			typescript: {},
+		},
+	},
+	ignorePatterns: [".eslintrc.js", "dist", "server", "node_modules", "coverage"],
 	rules: {
 		"@typescript-eslint/interface-name-prefix": "off",
 		"@typescript-eslint/explicit-function-return-type": "off",
 		"@typescript-eslint/explicit-module-boundary-types": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 	},
+	// React
+	"react/prop-types": "off",
+	"react/react-in-jsx-scope": "off",
+	"react-hooks/rules-of-hooks": "error",
+	"react-hooks/exhaustive-deps": "warn",
+	"import/order": [
+		"error",
+		{
+			groups: ["builtin", "external", "internal", ["parent", "sibling"], "index"],
+			"newlines-between": "always",
+			alphabetize: {
+				order: "asc",
+			},
+		},
+	],
 };
