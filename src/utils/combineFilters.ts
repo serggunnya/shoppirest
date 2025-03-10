b1cfdb90b2a7435c77e1b616c25cb4d7e2297540
serggunnya <baranov.serg.it@gmail.com>
2025-03-10 18:24:38 +0500

    refactor co,bineFilters

--- src/utils/combineFilters.ts
+++ src/utils/combineFilters.ts
@@ -1,61 +1,85 @@
 import { Prisma } from "@prisma/client";
 
-const combineFilters = (cat_id: number, filters: { [key: string]: any }): Prisma.Sql => {
-	const keys = Object.keys(filters);
-	const conditions: Prisma.Sql[] = [Prisma.empty];
+import {
+	AttributeType,
+	IFacets,
+	IFilters,
+	MultiValue,
+	NumericValue,
+} from "../product/interfaces/product.interface";
 
-	for (let i = 0; i < keys.length; i++) {
-		const attr = keys[i];
+const createMultiValueCondition = (
+	attr: string,
+	filterValue: MultiValue,
+	castType: Prisma.Sql,
+): Prisma.Sql => {
+	return Prisma.sql`(p.properties ->> ${attr})::${castType} IN (${Prisma.join(filterValue.val)})`;
+};
 
-		if (attr !== "price") {
-			if (filters[attr]?.type === 0) {
-				const query = Prisma.sql`
-					(p.properties ->> ${attr})::boolean IN (${Prisma.join(filters[attr].val)})
-				`;
-				conditions.push(query);
-			} else if (filters[attr]?.type === 1) {
-				const query = Prisma.sql`
-					(p.properties ->> ${attr})::text IN (${Prisma.join(filters[attr].val)})
-				`;
-				conditions.push(query);
-			} else if (filters[attr]?.type === 2) {
-				const query = Prisma.sql`
-					${
-						filters[attr]?.min
-							? Prisma.sql`(p.properties ->> ${attr})::int >= ${filters[attr].min}`
-							: Prisma.empty
-					}
-					${
-						filters[attr]?.max
-							? Prisma.sql`(p.properties ->> ${attr})::int <= ${filters[attr].max}`
-							: Prisma.empty
-					}
-				`;
-				conditions.push(query);
-			} else {
-				const query = Prisma.sql`
-					${
-						filters[attr]?.min
-							? Prisma.sql`(p.properties ->> ${attr})::float >= ${filters[attr].min}`
-							: Prisma.empty
-					}
-					${
-						filters[attr]?.max
-							? Prisma.sql`(p.properties ->> ${attr})::float <= ${filters[attr].max}`
-							: Prisma.empty
-					}
-				`;
-				conditions.push(query);
+const createNumericCondition = (
+	attr: string,
+	filterValue: NumericValue,
+	castType: Prisma.Sql,
+): Prisma.Sql => {
+	return Prisma.sql`
+		${
+			filterValue?.min
+				? Prisma.sql`(p.properties ->> ${attr})::${castType} >= ${filterValue.min}`
+				: Prisma.empty
+		}
+		${
+			filterValue?.max
+				? Prisma.sql`(p.properties ->> ${attr})::${castType} <= ${filterValue.max}`
+				: Prisma.empty
+		}
+	`;
+};
+
+const createPriceCondition = (filterValue: NumericValue): Prisma.Sql => {
+	return Prisma.sql`
+		${filterValue?.min ? Prisma.sql`AND p.price >= ${filterValue.min}` : Prisma.empty}
+		${filterValue?.max ? Prisma.sql`AND p.price <= ${filterValue.max}` : Prisma.empty}
+	`;
+};
+
+const combineFilters = (catId: number, filters: IFilters, attrMap: IFacets): Prisma.Sql => {
+	try {
+		const keys = Object.keys(filters);
+
+		const conditions: Prisma.Sql[] = [Prisma.empty];
+		for (const attr of keys) {
+			if (attr === "price") continue;
+
+			const value = filters[attr];
+
+			switch (attrMap[attr]?.type) {
+				case AttributeType.BOOLEAN:
+					conditions.push(
+						createMultiValueCondition(attr, value as MultiValue, Prisma.sql`boolean`),
+					);
+					break;
+				case AttributeType.TEXT:
+					conditions.push(createMultiValueCondition(attr, value as MultiValue, Prisma.sql`text`));
+					break;
+				case AttributeType.INTEGER:
+					conditions.push(createNumericCondition(attr, value as NumericValue, Prisma.sql`int`));
+					break;
+				default:
+					conditions.push(createNumericCondition(attr, value as NumericValue, Prisma.sql`float`));
+					break;
 			}
 		}
-	}
 
-	return Prisma.sql`
-		SELECT id FROM "Product" p where p.cat_id = ${cat_id}
-		${filters.price?.min ? Prisma.sql`AND p.price >= ${filters.price.min}` : Prisma.empty}
-		${filters.price?.max ? Prisma.sql`AND p.price <= ${filters.price.max}` : Prisma.empty}		
-		${Prisma.join(conditions, "AND")}
-	`;
+		return Prisma.sql`
+      SELECT id FROM "Product" p 
+      WHERE p.cat_id = ${catId}
+      ${createPriceCondition(filters.price as NumericValue)} 
+			${Prisma.join(conditions, " AND ")}
+    `;
+	} catch (error) {
+		console.error("Error in combineFilters:", error);
+		throw error;
+	}
 };
 
 export default combineFilters;
