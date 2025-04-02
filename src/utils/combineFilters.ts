657709a6167c75bfa9cec6824b2f822bdab09eb5
serggunnya <baranov.serg.it@gmail.com>
2025-04-02 13:02:44 +0500

    refactor combineFilters

--- src/utils/combineFilters.ts
+++ src/utils/combineFilters.ts
@@ -2,84 +2,87 @@ import { Prisma } from "@prisma/client";
 
 import {
 	AttributeType,
-	IFacets,
-	IFilters,
-	MultiValue,
-	NumericValue,
+	IAttributeMap,
+	ISearchFilters,
+	RangeValue,
+	SelectValue,
 } from "../product/interfaces/product.interface";
 
-const createMultiValueCondition = (
-	attr: string,
-	filterValue: MultiValue,
-	castType: Prisma.Sql,
+const combineFilters = (
+	categoryId: number,
+	searchFilters: ISearchFilters,
+	attributeMap: IAttributeMap,
 ): Prisma.Sql => {
-	return Prisma.sql`(p.properties ->> ${attr})::${castType} IN (${Prisma.join(filterValue.val)})`;
-};
-
-const createNumericCondition = (
-	attr: string,
-	filterValue: NumericValue,
-	castType: Prisma.Sql,
-): Prisma.Sql => {
-	return Prisma.sql`
-		${
-			filterValue?.min
-				? Prisma.sql`(p.properties ->> ${attr})::${castType} >= ${filterValue.min}`
-				: Prisma.empty
-		}
-		${
-			filterValue?.max
-				? Prisma.sql`(p.properties ->> ${attr})::${castType} <= ${filterValue.max}`
-				: Prisma.empty
-		}
-	`;
-};
-
-const createPriceCondition = (filterValue: NumericValue): Prisma.Sql => {
-	return Prisma.sql`
-		${filterValue?.min ? Prisma.sql`AND p.price >= ${filterValue.min}` : Prisma.empty}
-		${filterValue?.max ? Prisma.sql`AND p.price <= ${filterValue.max}` : Prisma.empty}
-	`;
-};
-
-const combineFilters = (catId: number, filters: IFilters, attrMap: IFacets): Prisma.Sql => {
 	try {
-		const keys = Object.keys(filters);
-
+		const keys = Object.keys(searchFilters);
 		const conditions: Prisma.Sql[] = [Prisma.empty];
-		for (const attr of keys) {
-			if (attr === "price") continue;
 
-			const value = filters[attr];
+		for (const alias of keys) {
+			const type = attributeMap[alias]?.type;
+			const data = searchFilters[alias];
 
-			switch (attrMap[attr]?.type) {
-				case AttributeType.BOOLEAN:
-					conditions.push(
-						createMultiValueCondition(attr, value as MultiValue, Prisma.sql`boolean`),
-					);
-					break;
-				case AttributeType.TEXT:
-					conditions.push(createMultiValueCondition(attr, value as MultiValue, Prisma.sql`text`));
-					break;
-				case AttributeType.INTEGER:
-					conditions.push(createNumericCondition(attr, value as NumericValue, Prisma.sql`int`));
-					break;
-				default:
-					conditions.push(createNumericCondition(attr, value as NumericValue, Prisma.sql`float`));
-					break;
+			if (type === AttributeType.NUMERIC) {
+				const condition = createRangeCondition(alias, data as RangeValue);
+				conditions.push(condition);
+			} else {
+				const condition = createSelectCondition(alias, data as SelectValue, type);
+				conditions.push(condition);
 			}
 		}
 
 		return Prisma.sql`
-      SELECT id FROM "Product" p 
-      WHERE p.cat_id = ${catId}
-      ${createPriceCondition(filters.price as NumericValue)} 
-			${Prisma.join(conditions, " AND ")}
+      SELECT id FROM "products" p 
+      WHERE p.category_id = ${categoryId} 
+			AND p.is_active = true AND p.stock > 0
+			${conditions.length > 0 ? Prisma.join(conditions, "") : Prisma.empty}	
     `;
 	} catch (error) {
 		console.error("Error in combineFilters:", error);
 		throw error;
 	}
+};
+
+const createSelectCondition = (
+	alias: string,
+	data: SelectValue,
+	type: AttributeType,
+): Prisma.Sql => {
+	let castType = Prisma.empty;
+
+	if (type === AttributeType.BOOLEAN) {
+		castType = Prisma.sql`boolean`;
+	} else if (type === AttributeType.NUMBER) {
+		castType = Prisma.sql`numeric`;
+	} else {
+		castType = Prisma.sql`text`;
+	}
+
+	return Prisma.sql`
+	 AND (p.properties -> ${alias} ->>'value')::${castType} IN (${Prisma.join(data.val)})`;
+};
+
+const createRangeCondition = (alias: string, data: RangeValue): Prisma.Sql => {
+	if (alias === "price") {
+		return Prisma.sql`
+			${data?.min ? Prisma.sql` AND p.price >= ${data.min}` : Prisma.empty}
+			${data?.max ? Prisma.sql` AND p.price <= ${data.max}` : Prisma.empty}
+		`;
+	}
+
+	const castType = Prisma.sql`numeric`;
+
+	return Prisma.sql`
+		${
+			data?.min
+				? Prisma.sql` AND (p.properties->${alias}->>'value')::${castType} >= ${data.min}`
+				: Prisma.empty
+		}
+		${
+			data?.max
+				? Prisma.sql` AND (p.properties->${alias}->>'value')::${castType} <= ${data.max}`
+				: Prisma.empty
+		}
+	`;
 };
 
 export default combineFilters;
