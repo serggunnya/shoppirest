b08c03034a50809347232542d88a899dac5c5b94
serggunnya <baranov.serg.it@gmail.com>
2025-04-11 16:13:41 +0500

    update eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,51 +4,75 @@ module.exports = {
 		project: "tsconfig.json",
 		tsconfigRootDir: __dirname,
 		sourceType: "module",
-		ecmaVersion: 2020,
+		ecmaVersion: 2023,
 	},
 	plugins: ["@typescript-eslint/eslint-plugin", "react", "prettier", "import", "react-hooks"],
 	extends: [
 		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react/recommended",
-		"plugin:prettier/recommended",
 		"plugin:import/typescript",
 		"plugin:import/errors",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:prettier/recommended",
 	],
 	root: true,
 	env: {
 		node: true,
 		jest: true,
-		es2020: true,
+		es2023: true,
 	},
 	settings: {
 		react: {
 			version: "detect",
 		},
 		"import/resolver": {
-			typescript: {},
+			typescript: {
+				alwaysTryTypes: true,
+			},
 		},
 	},
-	ignorePatterns: [".eslintrc.js", "dist", "server", "node_modules", "coverage"],
+	ignorePatterns: [".eslintrc.js", "dist", "server", "node_modules", "coverage", "build"],
 	rules: {
 		"@typescript-eslint/interface-name-prefix": "off",
 		"@typescript-eslint/explicit-function-return-type": "off",
 		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"@typescript-eslint/no-explicit-any": "off",
-	},
-	// React
-	"react/prop-types": "off",
-	"react/react-in-jsx-scope": "off",
-	"react-hooks/rules-of-hooks": "error",
-	"react-hooks/exhaustive-deps": "warn",
-	"import/order": [
-		"error",
-		{
-			groups: ["builtin", "external", "internal", ["parent", "sibling"], "index"],
-			"newlines-between": "always",
-			alphabetize: {
-				order: "asc",
+		"@typescript-eslint/no-explicit-any": "warn", // Changed to warn instead of off
+		"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+		"@typescript-eslint/no-floating-promises": "error",
+
+		// React
+		"react/prop-types": "off",
+		"react/react-in-jsx-scope": "off",
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+
+		// Import
+		"import/order": [
+			"error",
+			{
+				groups: [
+					"builtin",
+					"external",
+					"internal",
+					["parent", "sibling"],
+					"index",
+					"object",
+					"type",
+				],
+				"newlines-between": "always",
+				alphabetize: {
+					order: "asc",
+					caseInsensitive: true,
+				},
 			},
-		},
-	],
+		],
+		"import/no-duplicates": "error",
+
+		// General
+		"no-console": ["warn", { allow: ["warn", "error", "info"] }],
+		"prefer-const": "error",
+		eqeqeq: ["error", "always", { null: "ignore" }],
+	},
 };
