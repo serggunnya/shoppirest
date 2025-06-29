cf43437cd1b1f6aaf3f3afb8c703469deef7ea9f
serggunnya <baranov.serg.it@gmail.com>
2025-06-29 15:58:45 +0500

    add category service refactor

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Inject, Injectable } from "@nestjs/common";
+import { Cache } from "cache-manager";
 
 import { PrismaService } from "../prisma/prisma.service";
+import { ICategory } from "./interfaces/category.interface";
 
 @Injectable()
 export class CategoryService {
-	constructor(private prisma: PrismaService) {}
+	constructor(
+		private prisma: PrismaService,
+		@Inject(CACHE_MANAGER) private cacheService: Cache,
+	) {}
 
 	/**
 	 * Метод получения категорий
 	 * @param locale язык перевода
 	 * @returns категории
 	 */
-	async getAllCategories(locale: string) {
+	async getAllCategories(locale: string): Promise<ICategory[]> {
 		return await this.prisma.$queryRaw`
 			select 
 				c.id, c.slug, ct."name", ct.description, 
