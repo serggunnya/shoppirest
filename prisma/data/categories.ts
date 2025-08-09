55b2e70acb4752273fb8732a930aafae22ee6fa1
serggunnya <baranov.serg.it@gmail.com>
2025-08-09 11:26:00 +0500

    refactor seeding data

--- prisma/data/categories.ts
+++ prisma/data/categories.ts
@@ -3,7 +3,28 @@ import { Prisma } from "@prisma/client";
 export const categories: Prisma.categoriesCreateInput[] = [
 	{
 		// id: 1,
+		slug: "gadgets",
+		image: "/images/categories/gadgets.jpg",
+		is_active: true,
+		translations: {
+			create: [
+				{
+					locale: "ru",
+					name: "Гаджеты",
+					description: "Смартфоны, планшеты, умные-часы и другие гаджеты",
+				},
+				{
+					locale: "en",
+					name: "Gadgets",
+					description: "Smartphones, tablets, smartwatches and other gadgets",
+				},
+			],
+		},
+	},
+	{
+		// id: 2,
 		slug: "smartphones",
+		parent: { connect: { id: 1 } },
 		image: "/images/categories/smartphones.jpg",
 		is_active: true,
 		translations: {
@@ -22,8 +43,50 @@ export const categories: Prisma.categoriesCreateInput[] = [
 		},
 	},
 	{
-		// id: 2,
+		// id: 3,
+		slug: "tablets",
+		parent: { connect: { id: 1 } },
+		image: "/images/categories/tablets.jpg",
+		is_active: true,
+		translations: {
+			create: [
+				{
+					locale: "ru",
+					name: "Планшеты",
+					description: "Планшеты различных брендов",
+				},
+				{
+					locale: "en",
+					name: "Tablets",
+					description: "Tablets from various brands",
+				},
+			],
+		},
+	},
+	{
+		// id: 4,
+		slug: "home-appliances",
+		image: "/images/categories/home-appliances.jpg",
+		is_active: true,
+		translations: {
+			create: [
+				{
+					locale: "ru",
+					name: "Бытовая техника",
+					description: "Телевизоры, холодильники, микроволновки и другие бытовые приборы",
+				},
+				{
+					locale: "en",
+					name: "Home Appliances",
+					description: "Televisions, refrigerators, microwaves and other home appliances",
+				},
+			],
+		},
+	},
+	{
+		// id: 5,
 		slug: "tvs",
+		parent: { connect: { id: 4 } },
 		image: "/images/categories/tvs.jpg",
 		is_active: true,
 		translations: {
@@ -42,8 +105,29 @@ export const categories: Prisma.categoriesCreateInput[] = [
 		},
 	},
 	{
-		// id: 3,
+		// id: 6,
+		slug: "computers-and-laptops",
+		image: "/images/categories/computers-and-laptops.jpg",
+		is_active: true,
+		translations: {
+			create: [
+				{
+					locale: "ru",
+					name: "Компьютеры и ноутбуки",
+					description: "Компьютеры и ноутбуки для работы и развлечений",
+				},
+				{
+					locale: "en",
+					name: "Computers and Laptops",
+					description: "Computers and laptops for work and entertainment",
+				},
+			],
+		},
+	},
+	{
+		// id: 7,
 		slug: "laptops",
+		parent: { connect: { id: 6 } },
 		image: "/images/categories/laptops.jpg",
 		is_active: true,
 		translations: {
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -61,11 +61,7 @@ async function main() {
 	console.log(`Seeding finished.`);
 }
 
-main()
-	.catch((e) => {
-		console.error(e);
-		process.exit(1);
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-	});
+main().catch((e) => {
+	console.error(e);
+	process.exit(1);
+});
